f50c6c09c7ae159e44d04f0cfab4739e38061c96
ravil <ravil.yahyayev@gmail.com>
2023-05-07 19:42:14 +0400

    added proccess queries method

--- src/ListExample.java
+++ src/ListExample.java
@@ -7,15 +7,35 @@ import java.util.Scanner;
 public class ListExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         List<Integer> list = getList(sc);
+        int queryCount = sc.nextInt();
 
-
+        List<Integer> processedList = processQueries(sc, queryCount, list);
     }
 
-    public static List<Integer> getList(Scanner sc){
+    public static List<Integer> processQueries(Scanner sc, int queryCount, List<Integer> myList) {
+        for (int i = 0; i < queryCount; i++) {
+            switch (sc.next()) {
+                case "Insert" -> {
+                    int index = sc.nextInt();
+                    int element = sc.nextInt();
+                    myList.add(index, element);
+                }
+                case "Delete" -> {
+                    int index = sc.nextInt();
+                    myList.remove(index);
+                }
+                default -> throw new RuntimeException("There is no such query");
+            }
+        }
+        return myList;
+    }
+
+    public static List<Integer> getList(Scanner sc) {
         int size = sc.nextInt();
         List<Integer> myList = new ArrayList<>();
-        for (int i = 0 ; i<size ;i++){
+        for (int i = 0; i < size; i++) {
             myList.add(sc.nextInt());
         }
         return myList;
